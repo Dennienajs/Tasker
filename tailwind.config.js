b6bd1020f6c193f7967c154d89ebfbd1094f7317
Dennie Christensen <dennie.najs@hotmail.com>
2022-04-04 13:22:12 +0200

    updated tailwind defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    minHeight: {
+      "1/2": "50%",
+      "3/4": "75%",
+      "5/6": "83%",
+    },
+
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter var"],
+      },
+    },
   },
   plugins: [],
 };
