4164d31509ecbcc5f5471dace033a7b3457502cf
Dennie Christensen <dennie.najs@hotmail.com>
2022-04-05 13:33:02 +0200

    custom pipe declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskComponent } from './components/task/task.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
+import { PrettifyDatePipe } from './pipes/prettify-date.pipe';
 
 // Automagically added when using 'ng generate' or 'ng add'
 
@@ -22,7 +23,8 @@ import { FormsModule } from '@angular/forms';
     ButtonComponent,
     TasksComponent,
     TaskComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    PrettifyDatePipe
   ],
   imports: [
     BrowserModule,
