154c5e18f39958aff235cb1e708a4b54d47d760c
Dennie Christensen <dennie.najs@hotmail.com>
2022-04-05 14:43:53 +0200

    added darkmode toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UiService } from 'src/app/services/ui.service';
 import { Colors } from "../button/button.component";
+import { faMoon as moon, faSun as sun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +12,10 @@ import { Colors } from "../button/button.component";
 export class HeaderComponent implements OnInit {
   public colors = Colors;
   randomColor: Colors = Math.round(Math.random() * 7)
-  title = 'Angular Tasker';
+  title = 'Tasker';
   isShowingAddTask = false;
   subscription: Subscription;
+  darkmodeIcon = sun;
 
 
   constructor(private uiService: UiService) {
@@ -27,5 +29,30 @@ export class HeaderComponent implements OnInit {
   toggleAddTask = () =>
     this.uiService.toggleAddTask();
 
+  toggleDarkMode() {
+    toggleDarkMode();
+    if (this.darkmodeIcon === moon) {
+      this.darkmodeIcon = sun;
+    } else {
+      this.darkmodeIcon = moon;
+    }
+  }
 
+}
+
+function toggleDarkMode() {
+  console.log("toggleDarkMode()");
+  const key = "dark";
+
+  var root = document.getElementById("htmlRoot")!;
+
+  if (root.classList.contains(key)) {
+    root.classList.remove(key);
+    localStorage.setItem(key, "false");
+    console.log("removed dark");
+  } else {
+    root.classList.add(key);
+    localStorage.setItem(key, "true");
+    console.log("added dark");
+  }
 }
