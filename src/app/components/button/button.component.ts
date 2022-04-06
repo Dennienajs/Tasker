136659befe30ffd14efa0767a621b0fe422507b2
Dennie Christensen <dennie.najs@hotmail.com>
2022-04-06 09:19:42 +0200

    cleaned up button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+export enum Colors { Blue, Green, Cyan, Teal, Lime, Red, Pink, Purple }
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-
-
-
 export class ButtonComponent implements OnInit {
 
   @Input() type: string = "button";
@@ -15,56 +13,45 @@ export class ButtonComponent implements OnInit {
   @Input() color: Colors = Colors.Purple;
   @Output() click = new EventEmitter();
 
-  classes: string = "";
+  classes: string = "hover:animate-pulse bg-gradient-to-r hover:bg-gradient-to-br focus:outline-none focus:ring-1 font-medium rounded-lg text-sm px-5 py-2.5 text-center ";
 
   public Colors = Colors;
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
-    this.SetButtonClasses();
+    this.classes += GetButtonColorGradients(this.color);
   }
 
   onClick = () => {
-    console.log("BUTTON ONCLICK")
     this.click.emit();
   }
 
-  SetButtonClasses = () => {
+}
 
-    let all: string = "hover:animate-pulse bg-gradient-to-r hover:bg-gradient-to-br focus:outline-none focus:ring-1 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ";
-    let custom: string = "";
+const SetButtonColorGradient = (color: string, start: number, increase: number = 100, text: string = "white") => {
+  const from = `from-${color}-${start}`;
+  const via = `via-${color}-${start + increase}`;
+  const to = `to-${color}-${start + increase + increase}`;
 
-    switch (this.color) {
-      case Colors.Blue:
-        custom = "text-white from-blue-500 via-blue-600 to-blue-700";
-        break;
-      case Colors.Green:
-        custom = "text-white from-green-400 via-green-500 to-green-600"
-        break;
-      case Colors.Cyan:
-        custom = "text-white from-cyan-400 via-cyan-500 to-cyan-600"
-        break;
-      case Colors.Teal:
-        custom = "text-white from-teal-400 via-teal-500 to-teal-600"
-        break;
-      case Colors.Lime:
-        custom = "text-gray-900 from-lime-200 via-lime-400 to-lime-500"
-        break;
-      case Colors.Red:
-        custom = "text-white from-red-400 via-red-500 to-red-600"
-        break;
-      case Colors.Pink:
-        custom = "text-white from-pink-400 via-pink-500 to-pink-600"
-        break;
-      case Colors.Purple:
-        custom = "text-white from-purple-500 via-purple-600 to-purple-700"
-        break;
-    }
+  return `text-${text} ${from} ${via} ${to}`;
+}
 
-    this.classes = all + custom;
+const GetButtonColorGradients = (color: Colors) => {
+  switch (color) {
+    case Colors.Blue: return SetButtonColorGradient("blue", 500);
+    case Colors.Green: return SetButtonColorGradient("green", 400);
+    case Colors.Cyan: return SetButtonColorGradient("cyan", 400);
+    case Colors.Teal: return SetButtonColorGradient("teal", 400);
+    case Colors.Lime: return SetButtonColorGradient("lime", 200, 200, "gray-900")
+    case Colors.Red: return SetButtonColorGradient("red", 400);
+    case Colors.Pink: return SetButtonColorGradient("pink", 400);
+    case Colors.Purple: return SetButtonColorGradient("purple", 500);
+
+    default: return "text-white from-blue-500 via-blue-600 to-blue-700";
   }
 }
 
-export enum Colors { Blue, Green, Cyan, Teal, Lime, Red, Pink, Purple }
 
